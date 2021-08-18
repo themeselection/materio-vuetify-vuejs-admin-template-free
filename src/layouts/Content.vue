20eaf5556a8dee0223cec1d5e3103febba590f41
jdsolanki0001 <jdsolanki0001@gmail.com>
2021-08-18 12:17:31 +0530

    fix(footer): updated borken links

--- src/layouts/Content.vue
+++ src/layouts/Content.vue
@@ -75,7 +75,7 @@
             >ThemeSelection</a></span>
           <span class="d-sm-inline d-none">
             <a
-              href="https://themeselection.com/products/category/freebies/"
+              href="https://themeselection.com/products/category/download-free-admin-templates/"
               target="_blank"
               class="me-6 text--secondary text-decoration-none"
             >Freebies</a>
@@ -85,7 +85,7 @@
               class="me-6 text--secondary text-decoration-none"
             >Blog</a>
             <a
-              href="https://github.com/themeselection/materio-vuetify-vuejs-admin-template-free/blob/main/LICENSE.MD"
+              href="https://github.com/themeselection/materio-vuetify-vuejs-admin-template-free/blob/main/LICENSE"
               target="_blank"
               class="text--secondary text-decoration-none"
             >MIT Licence</a>
