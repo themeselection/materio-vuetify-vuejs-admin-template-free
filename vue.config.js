d5f84018dc1b2ed6d70bed6e0d3e6fc64a2150b1
JD <47495003+jd-0001@users.noreply.github.com>
2021-11-12 06:24:52 -0800

    feat(config): added lintOnSave option

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,7 @@ const { mergeSassVariables } = require('@vuetify/cli-plugin-utils')
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/demo/materio-vuetify-vuejs-admin-template-free/demo' : '/',
+  lintOnSave: false,
   transpileDependencies: ['vuetify'],
   chainWebpack: config => {
     const modules = ['vue-modules', 'vue', 'normal-modules', 'normal']
