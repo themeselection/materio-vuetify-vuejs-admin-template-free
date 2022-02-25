c3cc1bf94e8b659b7ee8a0cecc0cf405d334da61
JD <47495003+jd-0001@users.noreply.github.com>
2022-02-25 10:54:49 +0530

    fix(workflow): workflow and config updated due to pkg issue

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,6 @@ const { mergeSassVariables } = require('@vuetify/cli-plugin-utils')
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? '/materio-vuetify-vuejs-admin-template-free/demo' : '/',
   lintOnSave: false,
-  outputDir: 'dist/demo-new',
   transpileDependencies: ['vuetify'],
   chainWebpack: config => {
     const modules = ['vue-modules', 'vue', 'normal-modules', 'normal']
