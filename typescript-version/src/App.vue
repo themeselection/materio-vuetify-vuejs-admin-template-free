4cb149b3d4da4abfe4923631f6e979d2f8dee7f0
jd-solanki <jdsolanki0001@gmail.com>
2023-05-10 11:35:10 +0530

    feat: added upgrade to pro button

--- typescript-version/src/App.vue
+++ typescript-version/src/App.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+import UpgradeToPro from '@/components/UpgradeToPro.vue'
+</script>
+
 <template>
   <VApp>
     <RouterView />
+    <UpgradeToPro />
   </VApp>
 </template>
