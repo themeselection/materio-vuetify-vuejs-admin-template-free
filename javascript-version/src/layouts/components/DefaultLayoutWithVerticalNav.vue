47d235c5291853b81876d2207bb02fba1932bc15
jd-solanki <jdsolanki0001@gmail.com>
2024-01-08 11:46:28 +0530

    style: fix linting issue

--- javascript-version/src/layouts/components/DefaultLayoutWithVerticalNav.vue
+++ javascript-version/src/layouts/components/DefaultLayoutWithVerticalNav.vue
@@ -62,10 +62,13 @@ import UserProfile from '@/layouts/components/UserProfile.vue'
         to="/"
         class="app-logo app-title-wrapper"
       >
+
+        <!-- eslint-disable vue/no-v-html -->
         <div
           class="d-flex"
           v-html="logo"
         />
+        <!-- eslint-enable -->
 
         <h1 class="font-weight-medium leading-normal text-xl text-uppercase">
           Materio
